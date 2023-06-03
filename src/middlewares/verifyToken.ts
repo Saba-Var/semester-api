4ec9fda01f21ff32f34572f50bb472c1a0f89af2
Saba-Var <vartasashvili94@gmail.com>
2023-06-03 16:04:50 +0400

    fix: creae Request interfaces

--- src/middlewares/verifyToken.ts
+++ src/middlewares/verifyToken.ts
@@ -1,34 +1,49 @@
+import { ExtendedAuthRequest, AccessTokenPayload } from 'types'
 import { NextFunction, Response } from 'express'
-import { AuthReqBody } from './types'
 import jwt from 'jsonwebtoken'
 
-const verifyToken = (req: AuthReqBody, res: Response, next: NextFunction) => {
+const verifyToken = (
+  req: ExtendedAuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { authorization, Authorization } = req.headers
 
     const authHeader = authorization || Authorization
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(403).json({
-        message: 'Missing authorization headers. Unauthorized access!',
-      })
+    if (!!authHeader && typeof authHeader === 'string') {
+      if (!authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({
+          message: 'Missing authorization headers. Unauthorized access!',
+        })
+      }
+
+      const accessToken = authHeader.trim().split(' ')[1]
+
+      return jwt.verify(
+        accessToken,
+        process.env.ACCESS_TOKEN_SECRET!,
+        (error, JwtPayload) => {
+          if (error) {
+            return res.status(401).json({
+              message: 'User is not authorized to continue!',
+            })
+          }
+
+          const { email, id } = JwtPayload as AccessTokenPayload
+
+          req.body.currentUserId = id
+          req.body.currentUserEmail = email
+
+          return next()
+        }
+      )
     }
 
-    const accessToken = authHeader.trim().split(' ')[1]
-
-    return jwt.verify(
-      accessToken,
-      process.env.ACCESS_TOKEN_SECRET!,
-      (error) => {
-        if (error) {
-          return res.status(403).json({
-            message: 'User is not authorized to continue!',
-          })
-        }
-
-        return next()
-      }
-    )
+    return res.status(401).json({
+      message: 'Missing authorization headers. Unauthorized access!',
+    })
   } catch (error: any) {
     return res.status(500).json({ message: error.message })
   }
