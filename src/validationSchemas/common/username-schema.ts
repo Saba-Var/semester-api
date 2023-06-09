1206d718673955ac466c785402f94d984d46de1d
Saba-Var <vartasashvili94@gmail.com>
2023-06-09 19:46:40 +0400

    chore: change validation message texts

--- src/validationSchemas/common/username-schema.ts
+++ src/validationSchemas/common/username-schema.ts
@@ -3,11 +3,11 @@ import { check } from 'express-validator'
 const usernameSchema = check('username')
   .trim()
   .notEmpty()
-  .withMessage('Username is required')
+  .withMessage("'username' is required!")
   .isLength({
     min: 3,
     max: 30,
   })
-  .withMessage('Username should include at least 3 & max.30 characters')
+  .withMessage("'username' should be between 3 and 30 characters!")
 
 export default usernameSchema
--- src/validationSchemas/semesterSchema.ts
+++ src/validationSchemas/semesterSchema.ts
@@ -1,8 +1,18 @@
 import { check } from 'express-validator'
 
-const semesterValidationSchema = check('name')
-  .trim()
-  .notEmpty()
-  .withMessage('Semester name is required')
+const semesterValidationSchema = [
+  check('name').trim().notEmpty().withMessage('Semester name is required'),
 
+  check('startDate')
+    .trim()
+    .notEmpty()
+    .withMessage("'startDate' is required")
+    .isDate()
+    .withMessage("'startDate' should be a date"),
+
+  check('isCurrentSemester')
+    .optional()
+    .isBoolean()
+    .withMessage("'isCurrentSemester' should be a boolean"),
+]
 export default semesterValidationSchema
