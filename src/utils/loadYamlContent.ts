e7604dadbbbb49e7d4a39ce11eb24aed4c1f4974
Saba-Var <vartasashvili94@gmail.com>
2023-06-20 22:40:06 +0400

    docs: add user data model schema

--- src/utils/loadYamlContent.ts
+++ src/utils/loadYamlContent.ts
@@ -1,6 +1,10 @@
 import YAML from 'yamljs'
 
 export const loadYamlContent = (path: string, key?: string) => {
-  const yaml = YAML.load(`./src/swaggerDocs/${path}`)
-  return key ? yaml[key] : yaml
+  try {
+    const yaml = YAML.load(`./src/swaggerDocs/${path}`)
+    return key ? yaml[key] : yaml
+  } catch (error) {
+    return {}
+  }
 }
