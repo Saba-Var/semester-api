a626cbdf76646fa1a8fbb45ad11c5f7cc09a54e6
Saba-Var <vartasashvili94@gmail.com>
2023-06-28 09:32:31 +0400

    fix: move server creation logic into server.ts file

--- src/middlewares/swaggerDocSetup.ts
+++ src/middlewares/swaggerDocSetup.ts
@@ -1,4 +1,3 @@
-import { loadYamlFile, appendYamlFiles } from 'utils'
 import SwaggerUI from 'swagger-ui-express'
 
 export const swaggerDocSetup = () => {
@@ -7,48 +6,7 @@ export const swaggerDocSetup = () => {
     customSiteTitle: 'Semester API Specs',
   }
 
-  const swaggerDocument = {
-    openapi: '3.0.0',
-    produces: ['application/json'],
-    info: loadYamlFile('info.yaml'),
-    servers: loadYamlFile('servers.yaml'),
-    components: {
-      securitySchemes: loadYamlFile('securitySchemes.yaml'),
-      responses: loadYamlFile('responses.yaml'),
-      schemas: {},
-      examples: {},
-    },
-    paths: {},
-  }
-
-  const schemaYamlFiles = ['User', 'Semester', 'LearningActivity']
-  appendYamlFiles(
-    swaggerDocument.components.schemas,
-    'schemas',
-    schemaYamlFiles
-  )
-
-  const controllerYamlFiles = [
-    'authentication',
-    'user',
-    'semesters',
-    'learningActivities',
-  ]
-  appendYamlFiles(swaggerDocument.paths, 'controllers', controllerYamlFiles)
-
-  const partialYamlFiles = ['SemesterPartial', 'LearningActivityPartial']
-  appendYamlFiles(
-    swaggerDocument.components.schemas,
-    'schemas/partials',
-    partialYamlFiles
-  )
-
-  const examplesYamlFiles = ['LearningActivitiesExample']
-  appendYamlFiles(
-    swaggerDocument.components.examples,
-    'examples',
-    examplesYamlFiles
-  )
+  const swaggerDocument = {}
 
   return SwaggerUI.setup(swaggerDocument, options)
 }
---
+++ src/server.ts
@@ -0,0 +1,61 @@
+import express, { type RequestHandler } from 'express'
+import SwaggerUI from 'swagger-ui-express'
+import cookieParser from 'cookie-parser'
+import { connectToMongo } from 'config'
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv'
+import path from 'path'
+import cors from 'cors'
+import {
+  i18nextMiddleware,
+  swaggerDocSetup,
+  errorHandler,
+  verifyToken,
+  authLimiter,
+} from 'middlewares'
+import {
+  learningActivitiesRouter,
+  semesterRouter,
+  authRouter,
+  userRouter,
+} from 'routes'
+
+const server = express()
+
+dotenv.config()
+
+server.use(i18nextMiddleware)
+server.use(bodyParser.json())
+server.use(cookieParser())
+
+server.use(
+  cors({
+    origin: process.env.FRONTEND_URI!,
+    credentials: true,
+  })
+)
+
+server.use(authLimiter)
+
+server.use('/api-docs', SwaggerUI.serve, swaggerDocSetup())
+
+server.use('/api/authentication', authRouter)
+
+server.use(verifyToken as unknown as RequestHandler)
+
+server.use('/api/learning-activities', learningActivitiesRouter)
+server.use('/api/semesters', semesterRouter)
+server.use('/api/user', userRouter)
+
+server.set('view engine', 'pug')
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(errorHandler)
+
+const port = process.env.SERVER_PORT || 4000
+server.listen(port, async () => {
+  await connectToMongo()
+  console.log(`Server started on port ${port}`)
+})
+
+export default server
