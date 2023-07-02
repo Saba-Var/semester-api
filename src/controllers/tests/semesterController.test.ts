9e777f3ab5a38c5304556cc286ae74543acc4b2d
Saba-Var <vartasashvili94@gmail.com>
2023-07-02 11:05:25 +0400

    tests: get routes of semester

--- src/controllers/tests/semesterController.test.ts
+++ src/controllers/tests/semesterController.test.ts
@@ -1,7 +1,15 @@
-import { createSemesterRequest, userInfoPrivateRequest } from 'requests'
 import { NEW_SEMESTER_DATA_1, NEW_SEMESTER_DATA_2 } from './data'
+import { testingAuthStore } from 'store'
+import {
+  allSemestersDataRequest,
+  userInfoPrivateRequest,
+  oneSemesterDataRequest,
+  createSemesterRequest,
+} from 'requests'
 
 describe('Semester Controller', () => {
+  let newSemesterId: string
+
   describe('Create new Semester - POST /api/semesters', () => {
     it('Should return 422 if invalid data provided', async () => {
       const { status, body } = await createSemesterRequest({
@@ -24,11 +32,11 @@ describe('Semester Controller', () => {
       const userResponse = await userInfoPrivateRequest()
       expect(userResponse.body.semesters).toEqual([body._id])
       expect(userResponse.body.activeSemester).toBe(body._id)
+      newSemesterId = body._id
     })
 
     it('Should return 409 if the semester with the given name already exists', async () => {
       const { status, body } = await createSemesterRequest(NEW_SEMESTER_DATA_1)
-
       expect(status).toBe(409)
       expect(body.message).toBe(
         `Semester with this name '${NEW_SEMESTER_DATA_1.name}' already exists!`
@@ -37,11 +45,63 @@ describe('Semester Controller', () => {
 
     it('Should return 409 when creating new semester while user already has got an active semester', async () => {
       const { status, body } = await createSemesterRequest(NEW_SEMESTER_DATA_2)
-
       expect(status).toBe(409)
       expect(body.message).toBe(
         'You already have an active semester! End it and create a new one.'
       )
+    })
+  })
+
+  describe('Get all semesters list - GET /api/semesters', () => {
+    it('Should return 200 if semesters list of the user was fetched successfully', async () => {
+      const { status, body } = await allSemestersDataRequest()
+      expect(status).toBe(200)
+      expect(body).toHaveLength(1)
+      expect(body[0]).toEqual({
+        ...NEW_SEMESTER_DATA_1,
+        _id: newSemesterId,
+        user: testingAuthStore.currentUserId,
+        endDate: null,
+        isCurrentSemester: true,
+        learningActivities: [],
+        startDate: expect.any(String),
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String),
+      })
+    })
+  })
+
+  describe('Get one semester data by id', () => {
+    it('Should return 422 if the provided semester id is invalid', async () => {
+      const { status, body } = await oneSemesterDataRequest('invalidId')
+      expect(status).toBe(422)
+      expect(body.errors.id).toEqual([
+        'Invalid id param. Provide a valid mongoDB id.',
+      ])
+    })
+
+    it("Should return 404 if the semester with the given id doesn't exist", async () => {
+      const { status, body } = await oneSemesterDataRequest(
+        '5f8e9d8b4b2a3c1f4c7a5a1c'
+      )
+      expect(status).toBe(404)
+      expect(body.message).toBe('Semester not found!')
+    })
+
+    it('Should return 200 if the semester data was fetched successfully', async () => {
+      const { status, body } = await oneSemesterDataRequest(newSemesterId)
+      expect(status).toBe(200)
+      expect(body).toEqual({
+        ...NEW_SEMESTER_DATA_1,
+        _id: newSemesterId,
+        user: testingAuthStore.currentUserId,
+        endDate: null,
+        isCurrentSemester: true,
+        learningActivities: [],
+        startDate: expect.any(String),
+        updatedAt: expect.any(String),
+        createdAt: expect.any(String),
+      })
     })
   })
 })
--- src/requests/testing/semester.ts
+++ src/requests/testing/semester.ts
@@ -5,3 +5,9 @@ const { privateRequests } = superTestMethods
 
 export const createSemesterRequest = async (data: NewSemesterData) =>
   privateRequests.post('/api/semesters', data)
+
+export const allSemestersDataRequest = async () =>
+  privateRequests.get('/api/semesters')
+
+export const oneSemesterDataRequest = async (id: string) =>
+  privateRequests.get(`/api/semesters/${id}`)
