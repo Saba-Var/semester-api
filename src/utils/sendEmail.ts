771cd621914341a234604ff1e66f58fc25f8cbca
Saba-Var <vartasashvili94@gmail.com>
2023-08-23 08:57:11 +0400

    chore: generate token query param name conditionally

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -57,7 +57,9 @@ export const sendEmail = async (
 
   const redirectUri = `${process.env.FRONTEND_URI!}${
     languageCookie === 'en' ? '/en' : ''
-  }/${pageUri}?emailToken=${token}`
+  }/${pageUri}?${
+    emailTemplateType === 'change-email' ? 'emailToken' : 'token'
+  }=${token}`
 
   const html = pug.renderFile(
     path.join(__dirname, `../views/emails/templates/${emailTemplateType}.pug`),
